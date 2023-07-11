b69f319669b39f0c25e853f151a4404bc42d91dc
MarwanMohamed12 <138940689+MarwanMohamed12@users.noreply.github.com>
2023-07-11 22:58:37 +0300

    Update multiplier_controller.v

--- final/codes/multiplier_controller.v
+++ final/codes/multiplier_controller.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 module multiplier_controller
 (
     input clk, reset_a, 
@@ -28,33 +27,33 @@ module multiplier_controller
     
     always @(*)
     begin
-        state_out = idle;
-        input_sel = 2'b00;
-        shift_sel = 2'b00;
-        done =0;
-        clk_ena = 0;
-        sclr_n = 0;
+        state_out <= idle;
+        input_sel <= 2'b00;
+        shift_sel <= 2'b00;
+        done <=0;
+        clk_ena <= 0;
+        sclr_n <= 0;
     
         case (current_state)
         idle:
         begin
             if(start == 1 )
             begin
-                state_out = lsb;
-                input_sel = 2'bxx;
-                shift_sel = 2'bxx;
-                done = 0;
-                clk_ena = 1;
-                sclr_n = 0;
+                state_out <= lsb;
+                input_sel <= 2'bxx;
+                shift_sel <= 2'bxx;
+                done <= 0;
+                clk_ena <= 1;
+                sclr_n <= 0;
             end
             else 
             begin
-                state_out = idle;
-                input_sel = 2'bxx;
-                shift_sel = 2'bxx;
-                done = 0;
-                clk_ena = 0;
-                sclr_n = 1;
+                state_out <= idle;
+                input_sel <= 2'bxx;
+                shift_sel <= 2'bxx;
+                done <= 0;
+                clk_ena <= 0;
+                sclr_n <= 1;
             end
         end
             
@@ -62,119 +61,119 @@ module multiplier_controller
         begin
             if(start == 0 && count == 2'b00 )
             begin
-                state_out = mid;
-                input_sel = 2'b00;
-                shift_sel = 2'b00;
-                done = 0;
-                clk_ena = 1;
-                sclr_n = 1;
+                state_out <= mid;
+                input_sel <= 2'b00;
+                shift_sel <= 2'b00;
+                done <= 0;
+                clk_ena <= 1;
+                sclr_n <= 1;
             end
             else
             begin
-                state_out = err;
-                input_sel = 2'bxx;
-                shift_sel = 2'bxx;
-                done = 0;
-                clk_ena = 0;
-                sclr_n = 1;
+                state_out <= err;
+                input_sel <= 2'bxx;
+                shift_sel <= 2'bxx;
+                done <= 0;
+                clk_ena <= 0;
+                sclr_n <= 1;
             end
         end
              mid:
                begin
                    if(start == 0 && count == 2'b01 )
                    begin
-                       state_out = mid;
-                       input_sel = 2'b10;
-                       shift_sel = 2'b01;
-                       done = 0;
-                       clk_ena = 1;
-                       sclr_n = 1;
+                       state_out <= mid;
+                       input_sel <= 2'b10;
+                       shift_sel <= 2'b01;
+                       done <= 0;
+                       clk_ena <= 1;
+                       sclr_n <= 1;
                    end
                    else if(start ==0 && count == 2'b10 )
                    begin
-                       state_out = msb;
-                       input_sel = 2'b10;
-                       shift_sel = 2'b01;
-                       done = 0;
-                       clk_ena = 1;
-                       sclr_n = 1;
+                       state_out <= msb;
+                       input_sel <= 2'b10;
+                       shift_sel <= 2'b01;
+                       done <= 0;
+                       clk_ena <= 1;
+                       sclr_n <= 1;
                    end
                    else 
                    begin
-                      state_out = err;
-                      input_sel = 2'bxx;
-                      shift_sel = 2'bxx;
-                      done = 0;
-                      clk_ena = 0;
-                      sclr_n = 1;
+                      state_out <= err;
+                      input_sel <= 2'bxx;
+                      shift_sel <= 2'bxx;
+                      done <= 0;
+                      clk_ena <= 0;
+                      sclr_n <= 1;
                    end
               end
               msb:
               begin
                  if(start ==0 && count == 2'b11 )
                  begin
-                     state_out = calc_done;
-                     input_sel = 2'b11;
-                     shift_sel = 2'b10;
-                     done = 0;
-                     clk_ena = 1;
-                     sclr_n = 1;
+                     state_out <= calc_done;
+                     input_sel <= 2'b11;
+                     shift_sel <= 2'b10;
+                     done <= 0;
+                     clk_ena <= 1;
+                     sclr_n <= 1;
                  end
                  else 
                  begin
-                    state_out = err;
-                    input_sel = 2'bxx;
-                    shift_sel = 2'bxx;
-                    done = 0;
-                    clk_ena = 0;
-                    sclr_n = 1;
+                    state_out <= err;
+                    input_sel <= 2'bxx;
+                    shift_sel <= 2'bxx;
+                    done <= 0;
+                    clk_ena <= 0;
+                    sclr_n <= 1;
                  end
             end
             calc_done:
               begin
                  if(start)
                  begin
-                     state_out = err;
-                     input_sel = 2'bxx;
-                     shift_sel = 2'bxx;
-                     done = 0;
-                     clk_ena = 1;
-                     sclr_n = 1;
+                     state_out <= err;
+                     input_sel <= 2'bxx;
+                     shift_sel <= 2'bxx;
+                     done <= 0;
+                     clk_ena <= 1;
+                     sclr_n <= 1;
                  end
                  else 
                  begin
-                    state_out = idle;
-                    input_sel = 2'bxx;
-                    shift_sel = 2'bxx;
-                    done = 1;
-                    clk_ena = 0;
-                    sclr_n = 1;
+                    state_out <= idle;
+                    input_sel <= 2'bxx;
+                    shift_sel <= 2'bxx;
+                    done <= 1;
+                    clk_ena <= 0;
+                    sclr_n <= 1;
                  end
             end
       err:
           begin
              if(start == 0)
              begin
-                 state_out = err;
-                 input_sel = 2'bxx;
-                 shift_sel = 2'bxx;
-                 done = 0;
-                 clk_ena = 0;
-                 sclr_n = 1;
+                 state_out <= err;
+                 input_sel <= 2'bxx;
+                 shift_sel <= 2'bxx;
+                 done <= 0;
+                 clk_ena <= 0;
+                 sclr_n <= 1;
              end
              else 
              begin
-                state_out = lsb;
-                input_sel = 2'bxx;
-                shift_sel = 2'bxx;
-                done = 0;
-                clk_ena = 1;
-                sclr_n = 0;
+                state_out <= lsb;
+                input_sel <= 2'bxx;
+                shift_sel <= 2'bxx;
+                done <= 0;
+                clk_ena <= 1;
+                sclr_n <= 0;
              end
          end
          default:
          begin
-            state_out = idle;
+            state_out <= idle;
           end            
         endcase
     end
